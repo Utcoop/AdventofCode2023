ea6e0d035085d404250d488dd80cae5157c81fb3
Jackson Becker <jacksonbckr13@gmail.com>
2023-12-15 13:40:09 -0800

    Commented and refactored.

--- Day 2/AdventofCode2.java
+++ Day 2/AdventofCode2.java
@@ -4,49 +4,50 @@ import java.util.Scanner;
 public class AdventofCode2 {
     public static String path = "C:\\Users\\jacks\\Projects\\Advent of Code\\Day 2\\Advent_2.txt";
     public static int games;
+    
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(path));
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             System.out.println(line);
-            String[] arrOfStr = line.split("[,:;]");
-            String[] tempArr;
-            String[] arr;
+            String[] arrOfStr = line.split("[,:;]"); //Split the line into sections based on these characters
+            String[] tempArr; //To store one index of arr of star split by the color and value of the number 
+            String[] arr; //To store the game number and word game if we find the game is possible
             int tempNum;
             for (int i = 0; i < arrOfStr.length; i++){
-                if (arrOfStr[i].contains("green")) {
+                if (arrOfStr[i].contains("green")) { //If index contains green split that index of the string up by green and space
                     tempArr = arrOfStr[i].split("[ green]");
-                    tempNum = Integer.parseInt(tempArr[1]);
+                    tempNum = Integer.parseInt(tempArr[1]);  // The 1st index of tempArr always contains the number of cubes
                     if (tempNum > 13) {
-                        System.out.println("Too many green " + i);
+                        //System.out.println("Too many green " + i);
                         break;
                     }
-                } else if (arrOfStr[i].contains("red")) {
+                } else if (arrOfStr[i].contains("red")) { //If index contains green split that index of the string up by red and space
                     tempArr = arrOfStr[i].split("[ red]");
                     tempNum = Integer.parseInt(tempArr[1]);
                     if (tempNum > 12) {
-                        System.out.println("Too many red " + i);
+                        //System.out.println("Too many red " + i);
                         break;
                     }
-                } else if (arrOfStr[i].contains("blue")) {
+                } else if (arrOfStr[i].contains("blue")) { //If index contains green split that index of the string up by blue and space
                     tempArr = arrOfStr[i].split("[ blue]");
                     tempNum = Integer.parseInt(tempArr[1]);
                     if (tempNum > 14) {
-                        System.out.println("Too many Blue " + i);
+                        //System.out.println("Too many Blue " + i);
                         break;
                         
                     }    
                 }
-                System.out.println(i);
-                if (i==(arrOfStr.length-1)){
-                    System.out.println("Made it thorugh!");
-                    arr = arrOfStr[0].split("Game ");
-                    tempNum = Integer.parseInt(arr[1]);
-                    games = games + tempNum;
+                //System.out.println(i);
+                if (i==(arrOfStr.length-1)){ 
+                    //System.out.println("Made it thorugh!");
+                    arr = arrOfStr[0].split("Game "); // 0th index of arrOfStr always contains the game and game number
+                    tempNum = Integer.parseInt(arr[1]); //get game number
+                    games = games + tempNum;    //Sum the game nums
                 }
             }
-            System.out.println(games);
         }
+        System.out.println(games);
     }
 }
 
--- Day 2/AdventofCode2Part2.java
+++ Day 2/AdventofCode2Part2.java
@@ -3,46 +3,40 @@ import java.util.Scanner;
 
 public class AdventofCode2Part2 {
     public static String path = "C:\\Users\\jacks\\Projects\\Advent of Code\\Day 2\\Advent_2.txt";
-    public static int games;
+    public static int result;
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(path));
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             System.out.println(line);
-            String[] arrOfStr = line.split("[,:;]");
-            String[] tempArr;
-            String[] arr;
-            int[] grb = new int[]{0,0,0};
+            String[] arrOfStr = line.split("[,:;]"); //Split the line into sections based on these characters
+            String[] tempArr; //To store one index of arr of star split by the color and value of the number 
+            String[] arr; //To store the game number and word game if we find the game is possible
+            int[] grb = new int[]{0,0,0}; //Stores the largest number of each color cube found
             int tempNum;
             for (int i = 0; i < arrOfStr.length; i++){
-                if (arrOfStr[i].contains("green")) {
+                if (arrOfStr[i].contains("green")) { //If index contains "green" split that index of the string up by green and space
                     tempArr = arrOfStr[i].split("[ green]");
-                    tempNum = Integer.parseInt(tempArr[1]);
-                    if (tempNum > grb[0]){
+                    tempNum = Integer.parseInt(tempArr[1]);// The 1st index of tempArr always contains the number of cubes
+                    if (tempNum > grb[0]){ //if number of green cubes is greater than the stored value we have. Store the larger value
                         grb[0] = tempNum;
                     }
-                } else if (arrOfStr[i].contains("red")) {
+                } else if (arrOfStr[i].contains("red")) { //If index contains "red" split that index of the string up by red and space
                     tempArr = arrOfStr[i].split("[ red]");
                     tempNum = Integer.parseInt(tempArr[1]);
-                    if (tempNum > grb[1]){
+                    if (tempNum > grb[1]){//if number of red cubes is greater than the stored value we have. Store the larger value
                         grb[1] = tempNum;
                     }
-                } else if (arrOfStr[i].contains("blue")) {
+                } else if (arrOfStr[i].contains("blue")) { //If index contains "blue" split that index of the string up by blue and space
                     tempArr = arrOfStr[i].split("[ blue]");
                     tempNum = Integer.parseInt(tempArr[1]);
-                    if (tempNum > grb[2]){
+                    if (tempNum > grb[2]){//if number of blue cubes is greater than the stored value we have. Store the larger value
                         grb[2] = tempNum;
                     }
                 }
-                if (i==(arrOfStr.length-1)){
-                    System.out.println("Made it thorugh!");
-                    games += grb[0] * grb[1] * grb[2];
-                }
             }
+            result += grb[0] * grb[1] * grb[2]; //When we finish going through the array multiply the max number of each color we need by eachother and add it to result
         }
-        System.out.println(games);
+        System.out.println(result);
     }
 }
-
-// if (a.contains("game")){
-// }
